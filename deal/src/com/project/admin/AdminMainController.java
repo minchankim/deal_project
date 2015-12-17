a724d0c2da0498e845d3bdb29cd4665f1c51bbfe
sist <sist@sist-218>
2015-12-17 18:08:27 +0900

    admin

--- deal/src/com/project/admin/AdminMainController.java
+++ deal/src/com/project/admin/AdminMainController.java
@@ -1,15 +1,30 @@
 package com.project.admin;
 
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.member.SessionInfo;
 
 
 
 @Controller("admin.mainController")
 public class AdminMainController {
+	
+	
 	@RequestMapping(value="/admin", method=RequestMethod.GET)
-	public String main() throws Exception {
+	public String main(
+			//HttpSession session			
+			) throws Exception {
+		
+		/*SessionInfo info = (SessionInfo)session.getAttribute("member");
+		if(info==null){
+			return "redirect:/member/login.do";
+		}
+		*/
 		return ".adminMainLayout";
 	}
 }
